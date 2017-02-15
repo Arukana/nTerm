7684d2161b983fe831024475a21af92f8ed8526b
adjivas <adjivas@users.noreply.github.com>
2017-02-15 03:26:54 +0100

    support resize for thread

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@
 )]
 
 extern crate neko;
+extern crate libc;
 extern crate itertools;
 extern crate piston_window;
 extern crate graphics;
@@ -30,16 +31,22 @@ pub mod prelude;
 
 mod err;
 mod key;
+mod spawn;
+mod display;
 
 use std::ops::Mul;
 use std::io::Write;
 use std::path::Path;
+use std::thread;
+use std::sync::mpsc;
 use std::str;
+use std::mem;
 
 pub use neko::prelude as pty;
 pub use piston_window::*;
 use itertools::Itertools;
 
+pub use self::display::Display;
 pub use self::err::{NterminalError, Result};
 
 /// The sub-directory font.
@@ -47,9 +54,10 @@ const SPEC_SUBD_NCF: &'static str = "assets/fonts";
 
 pub struct Nterminal {
     window: PistonWindow,
-    glyph: Glyphs,
-    /// The shell interface.
-    shell: pty::Neko,
+    text: Glyphs,
+    speudo: pty::Master,
+    receive: mpsc::Receiver<Display>,
+    screen: Display,
     /// The font size.
     size: u8,
 }
@@ -71,35 +79,40 @@ impl Nterminal {
                                 .exit_on_esc(false)
                                 .build());
 
+        let (tx_master, rx_master) = mpsc::sync_channel::<pty::Master>(0);
+        let (tx_display, rx_display) = mpsc::sync_channel::<Display>(0);
+        thread::spawn(move || spawn::neko(tx_master, tx_display, winszed, font_size));
         Ok(Nterminal {
-            shell: try!(pty::Neko::new(None, None, None, Some(winszed))),
             size: font_size,
-            glyph: try!(Glyphs::new(Path::new(env!("CARGO_MANIFEST_DIR"))
+            text: try!(Glyphs::new(Path::new(env!("CARGO_MANIFEST_DIR"))
                                          .join(SPEC_SUBD_NCF)
                                          .join(font_name),
                                     window.factory.clone())),
             window: window,
+
+            speudo: rx_master.recv().unwrap(),
+            screen: rx_display.recv().unwrap(),
+            receive: rx_display,
         })
     }
 
     pub fn draw(&mut self, event: &piston_window::Event) {
-        self.shell.next();
+        let ref mut text = self.text;
         let font_size: usize = self.size as usize;
-        let window_size: &pty::Winszed = self.shell.get_window_size();
-        let width: usize = window_size.get_xpixel().checked_div(font_size as u32).unwrap_or_default() as usize*2;
-        let ref mut glyph = self.glyph;
-        let (ref pty_screen, ref screen): (&pty::PtyDisplay, &pty::Display) = self.shell.get_screen();
 
+
+        if let Ok(screen) = self.receive.try_recv() {
+            self.screen = screen;
+        }
+        let ref screen = self.screen;
         self.window.draw_2d(event, |c, g| {
             clear([1.0, 1.0, 1.0, 1.0], g);
-                pty_screen.into_iter()
-                    .zip(screen.into_iter())
-                    .collect::<Vec<(&pty::Character, pty::Character)>>()
-                    .as_slice()
-                    .chunks(width)
-                    .enumerate()
-                    .foreach(|(y, line): (usize, &[(&pty::Character, pty::Character)])| {
-                             line.into_iter().enumerate().foreach(|(x, &(&pty_character, character))| {
+            screen.into_iter()
+                  .enumerate()
+                  .foreach(|(y, line): (usize, &[(pty::Character, pty::Character)])| {
+                       line.into_iter()
+                           .enumerate()
+                          .foreach(|(x, &(pty_character, character))| {
                                  let (ref character, [fg_r, fg_g, rg_b]) = if character.is_null() {
                                      (pty_character.get_glyph().to_string(), pty_character.get_foreground())
                                  } else {
@@ -109,7 +122,7 @@ impl Nterminal {
                                  text::Text::new_color([fg_r as f32, fg_g as f32, rg_b as f32, 1.0], font_size as u32)
                                       .draw(
                                            character,
-                                           glyph,
+                                           text,
                                            &c.draw_state,
                                            transform, g
                                       );
@@ -129,10 +142,8 @@ impl Iterator for Nterminal {
                     self.draw(&event);
                     Some(())
                 },
-                Event::Input(Input::Press(Button::Keyboard(key))) => {
-                    let buf: [u8; 8] = key::Key::from(key).as_slice();
-
-                    let _ = self.shell.write(&buf.split_at(buf.iter().position(|c| b'\0'.eq(c)).unwrap_or_default()).0);
+                Event::Input(Input::Press(Button::Keyboard(code))) => unsafe {
+                    self.speudo.write(&mem::transmute::<piston_window::Key, [u8; 4]>(code)).expect("transmutation");
                     Some(())
                 },
                 Event::Input(Input::Press(Button::Mouse(mouse))) => {
@@ -145,11 +156,11 @@ impl Iterator for Nterminal {
 //                    self.shell.write(paste.as_bytes()).ok().and_then(|_| Some(()))
                     Some(())
                 },
-                Event::Input(Input::Resize(x, y)) => {
+                Event::Input(Input::Resize(x, y)) => unsafe {
                     let font_size: u32 = self.size as u32;
                     let (window_size_width, window_size_height): (u32, u32) =
                         (x, y);
-                    self.shell.set_window_size_with(
+                    libc::ioctl(libc::STDIN_FILENO, libc::TIOCSWINSZ,
                         &pty::Winszed {
                             ws_col: window_size_width.checked_div(font_size).unwrap_or_default() as u16*2,
                             ws_row: window_size_height.checked_div(font_size).unwrap_or_default() as u16,
@@ -160,6 +171,7 @@ impl Iterator for Nterminal {
                     Some(())
                 },
                 Event::Input(Input::Close) => {
+                    self.speudo.write(b"exit\n").expect("exit");
                     None
                 },
                 _ => Some(()),
